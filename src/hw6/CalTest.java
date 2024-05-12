604fd3d6347ff2244afd13965ac187aa6571cabd
kao <st941414hans@gmail.com>
2024-05-12 23:10:40 +0800

    開始寫作業摟

---
+++ src/hw6/CalTest.java
@@ -0,0 +1,7 @@
+package hw6;
+
+public class CalTest {
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+	}
+}
---
+++ src/hw6/Calculator.java
@@ -0,0 +1,10 @@
+package hw6;
+
+public class Calculator {
+	public Calculator() {
+		
+	}
+	public int powerXY(int x,int y) {
+		return (int)(Math.pow(x, y));
+	}
+}
