fd282cbcad87fb3ba99fcef2eca6fafc8b1ce9e9
kao <st941414hans@gmail.com>
2024-05-13 01:13:49 +0800

    修改hw6

--- src/hw6/Calculator.java
+++ src/hw6/Calculator.java
@@ -1,18 +1,33 @@
 package hw6;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
 	public Calculator() {
-		
+
 	}
-	public int powerXY(int x,int y) throws CalException {
-		if(x==0&&y==0) {
+
+	public int powerXY(int x, int y) throws CalException {
+		if (x == 0 && y == 0) {
 			throw new CalException("0的0次方沒有意義！");
-		}else if(y<0) {
+		} else if (y < 0) {
 			throw new CalException("次方為負值，結果回傳不為整數！");
-		}else {
-			return (int)(Math.pow(x, y));
+		} else {
+			return (int) (Math.pow(x, y));
+		}
+	}
+
+	public int getInt(String s, Scanner sc) {
+		while (true) {
+			System.out.println("請輸入" + s + "的值：");
+			try {
+				int num = sc.nextInt();
+				return num;
+			} catch (InputMismatchException i) {
+				System.out.println("輸入格式不正確");
+				sc.next();
+			}
 		}
 	}
 }
