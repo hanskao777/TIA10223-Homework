9240895165499729649f3460122a239d28f65ddd
kao <st941414hans@gmail.com>
2024-06-14 07:39:28 +0800

    hw9_2 更新 @#$% 應該快搞定了 吧

--- src/hw9/P2_BearBigNeedsMoney.java
+++ src/hw9/P2_BearBigNeedsMoney.java
@@ -1,12 +1,37 @@
 package hw9;
 
-public class P2_BearBigNeedsMoney {
-	public static void main(String[] args) {
-		Account ac = new Account();
-		Remit re = new Remit(ac);
-		Withdraw wi = new Withdraw(ac);
-		re.start();
-		wi.start();
+class Account {
+	int deposit;
+
+	synchronized public void remit(int a) {
+		while (deposit > 3000) {
+			System.out.println("夠多了，太多的話等等被拿去儲值手遊ˊ_>ˋ");
+			try {
+				wait();
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
+		deposit += a;
+		System.out.println("匯了" + a + "塊，存款有" + deposit + "塊");
+		notify();
+	}
+
+	synchronized public void withdraw(int money) {
+		while (deposit < money) {
+			System.out.println("沒錢啦，快點去找媽媽哭哭！");
+			try {
+				wait();
+			} catch (InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
+		deposit -= money;
+		System.out.println("領了" + money + "塊，存款剩下" + deposit + "塊");
+		if (deposit < 2000) {
+			System.out.println("媽媽快點給錢錢Q_Q");
+			notify();
+		}
 	}
 }
 
@@ -17,10 +42,12 @@ class Withdraw extends Thread {
 		this.ac = ac;
 	}
 
+
 	@Override
 	public void run() {
 		for (int i = 1; i <= 10; i++) {
 			ac.withdraw(1000);
+			System.out.println("領了" + i + "次");
 		}
 	}
 }
@@ -36,40 +63,17 @@ class Remit extends Thread {
 	public void run() {
 		for (int i = 1; i <= 10; i++) {
 			ac.remit(2000);
+			System.out.println("存了" + i + "次");
 		}
 	}
 }
 
-class Account {
-	int deposit;
-
-	synchronized public void withdraw(int money) {
-		while (deposit < money) {
-			System.out.println("沒錢啦，快點去找媽媽哭哭！");
-			try {
-				wait();
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-		}
-		deposit -= money;
-		System.out.println("領了" + money + "塊，存款剩下" + deposit+"塊");
-		if (deposit < 2000) {
-			notify();
-		}
-	}
-
-	synchronized public void remit(int money) {
-		while (deposit > 3000) {
-			System.out.println("夠多了，太多的話等等被拿去儲值手遊ˊ_>ˋ");
-			try {
-				wait();
-			} catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
-			deposit += money;
-			System.out.println("匯了"+money+"塊，存款有"+deposit+"塊");
-			notify();
-		}
+public class P2_BearBigNeedsMoney {
+	public static void main(String[] args) {
+		Account ac = new Account();
+		Remit re = new Remit(ac);
+		Withdraw wi = new Withdraw(ac);
+		re.start();
+		wi.start();
 	}
 }
